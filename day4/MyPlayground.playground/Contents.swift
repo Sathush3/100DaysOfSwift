a228df40d3c4b12893eb43c2d675801aec2b9a7f
Sathurshan Ramachandran <61341190+Sathush3@users.noreply.github.com>
2022-08-18 23:45:40 +0530

    Add files via upload

---
+++ day4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+//CHeckpoint 2
+
+var movies = ["Avatar","Inception","Sita Ramam","KGF","Vikram","KGF","Avatar"]
+
+print(movies.count)
+
+let setMovies = Set(movies)
+
+print(setMovies.count)
